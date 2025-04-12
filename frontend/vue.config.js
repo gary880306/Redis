cbf0291c8c93aae27bf0ef0e728ac8a240581908
gary.chen <ch66971542@gmail.com>
2025-04-12 23:12:16 +0800

    chore: vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
+const webpack = require('webpack')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 3000, // frontend port
+    port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // 指向 Nginx 負載均衡器
+        target: 'http://localhost:8080', // nginx
         changeOrigin: true
       }
     }
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false)
+      })
+    ]
   }
 })
